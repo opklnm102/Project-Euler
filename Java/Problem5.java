87b614337e68a3591f075d412831a727ad3577a2
DongHee Kim <opklnm102@gmail.com>
2017-03-30 11:13:01 +0900

    Fix: solve problem5 with euclidean

--- Java/Problem5.java
+++ Java/Problem5.java
@@ -1,12 +1,10 @@
-import java.util.stream.IntStream;
-
 /**
  * 1 ~ 10 사이의 어떤 수로도 나누어 떨어지는 가장 작은 수는 2520입니다.
  * 그러면 1 ~ 20 사이의 어떤 수로도 나누어 떨어지는 가장 작은 수는 얼마입니까?
- *
+ * <p>
  * 1 ~ 10의 최소공배수는 2520
  * 1 ~ 20의 최소공배수 구하기
- *
+ * <p>
  * 최소공배수를 구하려면 최대공약수 이용
  * 최대공약수를 구하려면 유클리드 호제법 이용
  */
@@ -14,15 +12,24 @@ public class Problem5 {
 
     public static void main(String[] args) {
 
-        int result = IntStream.iterate(11, x -> x + 1)
-                .filter(Problem5::isModFrom1to20)
-                .findFirst().getAsInt();
+        int max = 2520;
 
-        System.out.println(String.valueOf(result));  // 232792560
+        // 1 ~ 10까지는 알고 있으니 그것과 11 ~ 20의 최소공배수를 차례대로 구해간다
+        for (int i = 11; i < 21; i++) {
+            max = max / gcd(max, i) * i;
+        }
+
+        System.out.println(String.valueOf(max));  // 232792560
     }
 
-    public static boolean isModFrom1to20(int num) {
-        return IntStream.range(2, 20)
-                .noneMatch(x -> num % x != 0);
+    // 유클리드 호제법
+    public static int gcd(int a, int b) {
+        int mod = a % b;
+        while (mod > 0) {
+            a = b;
+            b = mod;
+            mod = a % b;
+        }
+        return b;
     }
 }
